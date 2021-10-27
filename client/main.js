6f1f54ba3471a837e43c94e43748f75b37b6d85b
adamf0x <adamfox96@gmail.com>
2021-10-27 13:04:06 -0700

    added comments etc

--- client/main.js
+++ client/main.js
@@ -7,10 +7,15 @@ import Land from './Land';
 // import "./styles.scss";
 
 const routes = (
+    /*Main.js will server as the "base" compoent for the rest of the application, Route components can be added in here to specify which components should be rendered for different routes 
+    https://reactrouter.com/web/guides/quick-start the documentation for react router is pretty good*/
     <Router>
         <Switch>
             <Route exact path="/" component={App}/>
-            <Route path="/landing" component={Land}/>
+            <Route path="/landing">
+                {/*what a route renders can also be specified like this, allowing you to pass props etc*/}
+                <Land testProp={"test"}></Land>
+            </Route>
         </Switch>
     </Router>
 );
