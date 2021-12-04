9ef4eea2987426bdc2b79034c81a856579bcc063
adamf0x <adamfox96@gmail.com>
2021-12-03 21:10:01 -0800

    gitstats info and final merge changes

--- client/routes.js
+++ client/routes.js
@@ -1,22 +1,22 @@
 import React from "react";
 
 //Layout
-import DashboardLayout from './Component/DashBoard/Layout';
+import DashboardLayout from "./Component/DashBoard/Layout";
 
 //Session Components
-import Land from './Land';
-import GroupedList3 from './Component/Session/Session';
-import Agenda from './Component/Agenda/agenda'
-import Vault from './Component/Vault/vault'
+import Land from "./Land";
+import GroupedList3 from "./Component/Session/Session";
+import Agenda from "./Component/Agenda/agenda";
+import Vault from "./Component/Vault/vault";
 
-import About from './About';
-import NoMatch from './NoMatch';
-import Testpg from './anothertestpg';
+import About from "./About";
+import NoMatch from "./NoMatch";
+import Testpg from "./anothertestpg";
 
-import LoggedinUsers from './Component/AdminComponents/LoggedInUsers';
-import RegisteredUsers from './Component/AdminComponents/RegisteredUsers';
-import Stats from './Component/AdminComponents/Stats/Stats';
-import Settings from './Component/AdminComponents/Settings/Settings'
+import LoggedinUsers from "./Component/AdminComponents/LoggedInUsers";
+import RegisteredUsers from "./Component/AdminComponents/RegisteredUsers";
+import Stats from "./Component/AdminComponents/Stats/Stats";
+import Settings from "./Component/AdminComponents/Settings/Settings";
 
 // // Declarative routing model
 // return (
@@ -29,76 +29,73 @@ import Settings from './Component/AdminComponents/Settings/Settings'
 
 //Programmatic routing model
 const routes = [
-    // { path: '/', element: <Land /> },
-    // {
-    //     path: 'about',
-    //     element: <About />,
-    //     children: [
-    //         {
-    //             path: '/about', element: <NoMatch />
-    //         }
-    //     ]
-    // },
-    {
-        path: '/',
-        element: <DashboardLayout />,
-        children: [
-            { index: true, element: <GroupedList3 /> },
-            {
-                path: '/agenda',
-                element: <Agenda />,
-            },
-            // {
-            //     path: ''
-            // },
-            {
-                path: '/vault',
-                element: <Vault />,
-                // children: [
-                //     { index: true, element: <Testpg/> },
-                // ]x
-            },
-            {
-                path: '/loggedinUsers',
-                element: <LoggedinUsers />
-            },
-            {
-                path: '/registeredusers',
-                element: <RegisteredUsers />
-            },
-            {
-                path: '/stats',
-                element: <Stats />
-            },
-            {
-                path: '/settings',
-                element: <Settings />
-            },
-            { path: "*", element: <NoMatch /> }
-        ]
-    },
+  // { path: '/', element: <Land /> },
+  // {
+  //     path: 'about',
+  //     element: <About />,
+  //     children: [
+  //         {
+  //             path: '/about', element: <NoMatch />
+  //         }
+  //     ]
+  // },
+  {
+    path: "/",
+    element: <DashboardLayout />,
+    children: [
+      { index: true, element: <GroupedList3 /> },
+      {
+        path: "/agenda",
+        element: <Agenda />,
+      },
+      // {
+      //     path: ''
+      // },
+      {
+        path: "/vault",
+        element: <Vault />,
+        // children: [
+        //     { index: true, element: <Testpg/> },
+        // ]x
+      },
+      {
+        path: "/loggedinUsers",
+        element: <LoggedinUsers />,
+      },
+      {
+        path: "/registeredusers",
+        element: <RegisteredUsers />,
+      },
+      {
+        path: "/stats",
+        element: <Stats />,
+      },
+      {
+        path: "/settings",
+        element: <Settings />,
+      },
+      { path: "*", element: <NoMatch /> },
+    ],
+  },
 
-    // },
-    // // {
-    //     path: '/about',
-    //     element: <About />,
+  // },
+  // // {
+  //     path: '/about',
+  //     element: <About />,
 
-    //     children: [
-    //         { index: true, element: <Testpg /> },
-    //         {
-    //             path: '/about',
-    //             element: <About />,
-    //             children: [
-    //                 { index: true, element: <About /> },
-    //             ]
-    //         },
-    //         { path: "*", element: <NoMatch /> }
-    //     ]
+  //     children: [
+  //         { index: true, element: <Testpg /> },
+  //         {
+  //             path: '/about',
+  //             element: <About />,
+  //             children: [
+  //                 { index: true, element: <About /> },
+  //             ]
+  //         },
+  //         { path: "*", element: <NoMatch /> }
+  //     ]
 
-    // },
-
-
-    { path: "*", element: <NoMatch /> }
+  // },
 
   { path: "*", element: <NoMatch /> },
 ];
