35be71a43eec240a3dc18d84ba0c56f87af960ba
rileyccc <rileyclark14@icloud.com>
2022-02-03 18:11:00 -0800

    create sequalize model of Administrator

---
+++ server/participant/models/Administrator.js
@@ -0,0 +1,26 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const db = require("../config/db");
+
+const Administrator = db.define(
+  "Administrator",
+  {
+    adminId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    freezeTableName: true,
+    timestamps: false,
+  }
+);
+
+module.exports = Administrator;
