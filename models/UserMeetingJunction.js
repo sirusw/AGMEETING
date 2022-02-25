07b37db6e1a203b65b0d6f96dcc786e34271cfeb
Khai Hung <hungluongkhai@gmail.com>
2022-02-24 21:33:01 -0800

    Update code with sequelize and passportJS

--- models/UserMeetingJunction.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize, DataTypes, Model } = require('sequelize');
-const db = require();
-
-const userMeetingJunction = Sequelize.define('userMeetingJunction', 
-{
-    meeting_id:{ type: DataTypes.INTEGER, references: { model: meeting, key:'id'}},
-    user_id:{type: DataTypes.INTEGER, references: { model: meeting, key:'id'}}
-
-});
--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,18 @@
+//db connection
 const { Sequelize } = require("sequelize");
+/*
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
 
-module.exports = new Sequelize(
-  "postgres://postgres:password@localhost:5432/local_agmeeting_database"
-);
+module.exports = new Sequelize("AGMEETING", "postgres", "admin", {
+    host: "localhost",
+    dialect: "postgres",
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+});
