be1f1b73c3d380ead4000f6a8cda99ccf0a58a31
ourinsou <ourinsou@gmail.com>
2022-03-04 18:42:17 -0800

    add usermeetingjunction migration

---
+++ server/migrations/20220225071115-create-usermeetingjunction.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('usermeetingjunctions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      meeting_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model:{
+            tableName: 'meeting'
+          },
+          key: 'id'
+        }
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model:{
+            tableName: 'client'
+          },
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('usermeetingjunctions');
+  }
+};
